0d3840b1a1c2189d8c9ee56ac05670a43853a32f
Himanshu Srivastava <srivastavahimanshu24@gmail.com>
2017-06-23 23:56:43 +0530

    alphabet rangoli

---
+++ alphabet_rangoli.py
@@ -0,0 +1,17 @@
+import string
+def print_rangoli(size):
+    middle = size -1
+    for i in range(size-1, 0, -1):
+        row = ['-'] * (2 * size - 1)
+        for j in range(size - i):
+            row[middle -j]= row[middle +j]= string.ascii_lowercase[j + i]
+        print '-'.join(row)
+    for i in range(0, size):
+        row =['-'] * (2* size - 1)
+        for j in range(0, size - i):
+            row[middle - j]= row[middle+j]= string.ascii_lowercase[j +i]
+        print '-'.join(row)
+        
+if __name__=='__main__':
+    n = int(raw_input)
+    print_rangoli(n)
